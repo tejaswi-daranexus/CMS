6522fe62c924635d6d3590c2ee4fed6170fd4b7e
tejaswi-daranexus <tejaswi@daranexus.in>
2026-05-06 14:05:39 +0530

    Removed Redis, cleaned test config, stable backend setup

--- src/test/java/com/cms/TestCmsBackendApplication.java
+++ src/test/java/com/cms/TestCmsBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 public class TestCmsBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(CmsBackendApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.run(CmsBackendApplication.class, args);
 	}
-
 }
